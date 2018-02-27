dd9288713f584a082e775a0831438c909c0e4549
Wu Minzhe <wuminzhe@126.com>
2018-02-27 09:13:13 +0800

    update

--- lib/eth_tool.rb
+++ lib/eth_tool.rb
@@ -2,22 +2,48 @@ require "eth_tool/version"
 require 'bip44'
 require 'ethereum.rb'
 require 'eth'
+require 'bigdecimal'
 
 module EthTool
+  @@gas_limit = 60000
+  @@gas_price = 5_000_000_000
+
   def self.rpc=(url)
     @@rpc = Ethereum::HttpClient.new(url)
   end
+
+  def self.rpc
+    @@rpc
+  end
+
+  def self.gas_limit=(gas_limit)
+    @@gas_limit = gas_limit
+  end
+
+  def self.gas_price=(gas_price)
+    @@gas_price = gas_price
+  end
+
+  def self.fee
+    BigDecimal(@@gas_limit) * BigDecimal(@@gas_price) / 10**18.to_f
+  end
+
+  class << self
+    attr_accessor :logger
+  end
+
+  ############################################################################
 
   def self.get_token_balance(address, token_contract_address, token_decimals)
     data = '0x70a08231' + padding(address) # Ethereum::Function.calc_id('balanceOf(address)') # 70a08231
   
     result = @@rpc.eth_call({to: token_contract_address, data: data})['result'].to_i(16)
-    result / (10**token_decimals.to_f)
+    BigDecimal(result) / BigDecimal(10**token_decimals)
   end
   
   def self.get_eth_balance(address)
     result = @@rpc.eth_get_balance(address)['result'].to_i(16)
-    result / (10**18.to_f)
+    BigDecimal(result) / BigDecimal(10**18)
   end
 
   def self.generate_raw_transaction(private_key, value, data, gas_limit, gas_price = nil, to = nil, nonce = nil)
@@ -35,7 +61,7 @@ module EthTool
       gas_limit: gas_limit,
       gas_price: gas_price_in_dec
     }
-    
+
     args[:value] = (value * 10**18).to_i if value
     args[:data] = data if data
     args[:to] = to if to
@@ -45,6 +71,11 @@ module EthTool
   end
 
   def self.transfer_token(private_key, token_contract_address, token_decimals, amount, gas_limit, gas_price, to)
+    if amount < (1.0/10**token_decimals)
+      logger.info "转账金额不能小于精度最小单位(#{token_contract_address}, #{token_decimals})"
+      return
+    end
+
     # 生成raw transaction
     amount_in_wei = (amount * (10**token_decimals)).to_i
     data = '0xa9059cbb' + padding(to) + padding(dec_to_hex(amount_in_wei)) # Ethereum::Function.calc_id('transfer(address,uint256)') # a9059cbb
@@ -54,25 +85,41 @@ module EthTool
   end
 
   def self.transfer_eth(private_key, amount, gas_limit, gas_price, to)
+    if amount < (1.0/10**18)
+      logger.info "转账金额不能小于以太坊最小单位"
+      return 
+    end
     rawtx = generate_raw_transaction(private_key, amount, nil, gas_limit, gas_price, to)
     @@rpc.eth_send_raw_transaction(rawtx)
   end
 
+  # 
   def self.sweep_token(private_key, token_contract_address, token_decimals, to)
     address = ::Eth::Key.new(priv: private_key).address
     token_balance = get_token_balance(address, token_contract_address, token_decimals)
-    gas_limit = 60000
-    gas_price = 5_000_000_000
-    transfer_token(private_key, token_contract_address, token_decimals, token_balance, gas_limit, gas_price, to)
+    return if token_balance == 0
+    transfer_token(private_key, token_contract_address, token_decimals, token_balance, @@gas_limit, @@gas_price, to)
   end
 
   def self.sweep_eth(private_key, to)
     address = ::Eth::Key.new(priv: private_key).address
     eth_balance = get_eth_balance(address)
-    gas_limit = 60000
-    gas_price = 5_000_000_000
-    amount = eth_balance - (gas_limit * gas_price / 10**18.to_f)
-    transfer_eth(private_key, amount, gas_limit, gas_price, to)
+    keep = BigDecimal(@@gas_limit) * BigDecimal(@@gas_price) / 10**18
+    amount = eth_balance - keep
+    return if amount <= 0
+    transfer_eth(private_key, amount, @@gas_limit, @@gas_price, to)
+  end
+
+  def self.fill_eth(private_key, to)
+    # 目标地址上现有eth
+    eth_balance = get_eth_balance(to)
+
+    # 目标地址上需要填充满这么多eth
+    amount = BigDecimal(@@gas_limit) * BigDecimal(@@gas_price) / 10**18
+
+    return unless eth_balance < amount
+    
+    transfer_eth(private_key, (amount-eth_balance), @@gas_limit, @@gas_price, to)
   end
 
   def self.generate_addresses_from_xprv(xprv, amount)
@@ -91,8 +138,53 @@ module EthTool
     return sub_wallet.private_key, sub_wallet.ethereum_address
   end
 
+  def self.wait_for_miner(txhash, timeout: 1200, step: 20)
+    start_time = Time.now
+    loop do
+      raise Timeout::Error if ((Time.now - start_time) > timeout)
+      return true if mined?(txhash)
+      sleep step
+    end
+  end
 
-  # helper methods
+  def self.mined?(txhash)
+    tx = @@rpc.eth_get_transaction_by_hash(txhash)["result"]
+    tx and (not tx['blockNumber'].nil?)
+  end
+
+  def self.confirmed?(txhash, number)
+    tx = @@rpc.eth_get_transaction_by_hash(txhash)["result"]
+
+    return false unless (tx and (not tx['blockNumber'].nil?))
+
+    block_number = tx['blockNumber'].to_i(16)
+    current_block_number = @@rpc.eth_block_number["result"].to_i(16)
+    confirmations = current_block_number - block_number
+
+    confirmations >= number
+  end
+
+  def self.confirmations(txhash)
+    tx = @@rpc.eth_get_transaction_by_hash(txhash)["result"]
+
+    return 0 unless (tx and (not tx['blockNumber'].nil?))
+
+    block_number = tx['blockNumber'].to_i(16)
+    current_block_number = @@rpc.eth_block_number["result"].to_i(16)
+    current_block_number - block_number
+  end
+
+  def self.token_transfer_success?(txhash)
+    tx = @@rpc.eth_get_transaction_by_hash(txhash)["result"]
+    return false unless (tx and (not tx['blockNumber'].nil?))
+
+    receipt = @@rpc.eth_get_transaction_receipt(txhash)["result"]
+    return false unless receipt
+
+    return false if (receipt['status'] && receipt['status'] == '0x0')
+  end
+
+  # helper methods #########################################################
 
   def self.dec_to_hex(value)
     '0x'+value.to_s(16)
